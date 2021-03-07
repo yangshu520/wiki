636ab96411493b47a3631bc49b1fc54850a8d591
yangshu520 <1300272778@qq.com>
2021-03-07 20:23:24 +0800

    增加接口测试文件接口

--- src/main/java/com/jiawa/wiki/Controller/TestController.java
+++ src/main/java/com/jiawa/wiki/Controller/TestController.java
@@ -1,7 +1,8 @@
 package com.jiawa.wiki.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //RestController 返回字符串或者JSON   @Controller //返回页面
 @RestController
@@ -19,5 +20,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello love";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello love ? Post="+name;
     }
 }
