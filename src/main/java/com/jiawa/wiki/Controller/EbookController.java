6cd6c2e2057e3388a17a5ec6835618b63fdd5ae0
yangshu520 <1300272778@qq.com>
2021-03-09 21:13:06 +0800

    完成电子书列表查询接口

--- src/main/java/com/jiawa/wiki/Controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/Controller/EbookController.java
@@ -1,7 +1,7 @@
 package com.jiawa.wiki.Controller;
 
-import com.jiawa.wiki.entity.Demo;
-import com.jiawa.wiki.service.DemoService;
+import com.jiawa.wiki.entity.Ebook;
+import com.jiawa.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/demo")
-public class DemoController {
+@RequestMapping("/ebook")
+public class EbookController {
     @Autowired
-    private DemoService demoService;
+    private EbookService ebookService;
 
     @GetMapping("/list1")
-    public List<Demo> list(){
-        return demoService.list();
+    public List<Ebook> list(){
+        return ebookService.list();
     }
 }
