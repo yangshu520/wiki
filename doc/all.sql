7e44dda53c19dddb0019ec4323391066f698690b
yangshu520 <1300272778@qq.com>
2021-03-09 21:06:45 +0800

    新增ebook表 并且生成了持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,1 +1,15 @@
-select * from test
+drop table if exists `ebook`;
+create table `ebook`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '阅读数',
+    `vote_count` int comment '点赞数',
+    primary key(`id`)
+) engine=innodb default charset=utf8mb4 comment='电子书';
+
+insert into `ebook` (id, name, description) values (1,'spring Boot 入门教程','零基础入门 JAVA开发');
