8b47a7f39eeeccf654c7087238d0a931c721636b
yangshu520 <1300272778@qq.com>
2021-03-10 21:28:02 +0800

    集成图标组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,8 +7,17 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
-createApp(App)
-    .use(store)
+//ant degin 图标库
+import * as Icons from "@ant-design/icons-vue"
+
+const app = createApp(App);
+app.use(store)
     .use(router)
     .use(Antd)
     .mount('#app')
+
+//全局使用图标
+const icons: any = Icons;
+for(const i in icons){
+    app.component(i,icons[i])
+}
